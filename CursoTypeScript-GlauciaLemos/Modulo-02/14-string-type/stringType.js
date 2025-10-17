17e910ac2b41366ce2d31e259eb35461a2f7f393
Guilherme França Fernandes <guilhermef.fernandes@qualitydigital.global>
2025-10-17 11:40:48 -0300

    TSC - Aula 14 - string

---
+++ CursoTypeScript-GlauciaLemos/Modulo-02/14-string-type/stringType.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Exemplo 01 - Single Quotes
+let fullName = 'Guilherme França Fernandes';
+console.log(fullName);
+// Exemplo 02 - Double Quotes
+let companyFunction = "intern developer";
+console.log(companyFunction);
+// Exemplo 03 - Back Ticks
+let companyName = 'Quality Digital';
+let dataEmployee = `Welcome ${fullName}! You will be an ${companyFunction} at ${companyName}`;
+console.log(dataEmployee);
+//# sourceMappingURL=stringType.js.map
---
+++ CursoTypeScript-GlauciaLemos/Modulo-02/14-string-type/stringType.ts
@@ -0,0 +1,12 @@
+// Exemplo 01 - Single Quotes
+let fullName: string = 'Guilherme França Fernandes';
+console.log(fullName);
+
+// Exemplo 02 - Double Quotes
+let companyFunction: string = "intern developer";
+console.log(companyFunction);
+
+// Exemplo 03 - Back Ticks
+let companyName: string = 'Quality Digital';
+let dataEmployee: string = `Welcome ${fullName}! You will be an ${companyFunction} at ${companyName}`;
+console.log(dataEmployee);
